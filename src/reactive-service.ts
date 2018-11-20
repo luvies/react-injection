b30c77410054b2b5080dc21bde337baa33e9ba8f
Luke Davies <lkdvs3@gmail.com>
2018-11-20 22:31:37 +0000

    Removed unused function

--- src/reactive-service.ts
+++ src/reactive-service.ts
@@ -1,11 +1,6 @@
 import { inject, injectable } from 'inversify';
 import { AfterFn, StateTracker, StateUpdater } from './state-tracker';
 
-export function isReactiveService<TState extends object = any>(service: unknown): service is ReactiveService<TState> {
-  // @ts-ignore
-  return typeof service === 'object' && service && service.isReactiveService;
-}
-
 @injectable()
 export abstract class ReactiveService<TState extends object> {
   protected abstract state: TState;
